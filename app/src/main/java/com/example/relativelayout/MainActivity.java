3631a485937af6420623ecd3ae4e5982e55e4ed0
safeerullah <safeerafridi9@gmail.com>
2020-11-21 03:21:59 +0500

    Initial commit

---
+++ app/src/main/java/com/example/relativelayout/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        Toast.makeText(this, "Resumed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Paused", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Destroyed", Toast.LENGTH_SHORT).show();
+    }
+}
